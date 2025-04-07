bbdef68a66f10d983a8cf86134569b18fe6c8652
Ondřej Műller <ondrej.muller@netglade.cz>
2025-04-07 15:45:09 +0200

    delete project

--- lib/isFileInArray.ts
+++
@@ -1,9 +0,0 @@
-
-export function isFileInArray(file: File, existingFiles: File[]) {
-    return existingFiles.some(
-        (existing) =>
-            existing.name === file.name &&
-            existing.size === file.size &&
-            existing.type === file.type
-    )
-}
